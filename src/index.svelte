7365fdb557bb96f7d0d9da69fc63ccf452f44daa
fairydhwen <fairydhwen@gmail.com>
2022-03-12 21:23:11 +0100

    initial template

---
+++ src/index.svelte
@@ -0,0 +1,18 @@
+  <script lang="ts">
+      const title: string = "svelte typescript template with parcel";
+  </script>
+  
+  <style lang="scss">
+    :global(body) {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    h1 {
+      font: normal 24pt Lato, "Helvetica Neue", Arial, Helvetica, sans-serif;
+      color: dimgray;
+      cursor: default;
+    }
+  </style>
+  
+  <h1>{title}</h1>
